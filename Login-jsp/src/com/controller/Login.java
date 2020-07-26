144d9987e0d876c1dee000b0478514f93a8dda20
yatheesha04 <42319204+yatheesha04@users.noreply.github.com>
2020-07-26 22:07:09 +0530

    attaaching data base

--- Login-jsp/src/com/controller/Login.java
+++ Login-jsp/src/com/controller/Login.java
@@ -1,12 +1,19 @@
 package com.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.bean.User;
+import com.dao.UserDao;
+import com.service.UserService;
 
 /**
  * Servlet implementation class Login
@@ -23,26 +30,48 @@ public class Login extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		// TODO Auto-generated method stub
 		String name=request.getParameter("uname");
+		
 		String password=request.getParameter("pass");
 		
-		if(name.equals("yatheesha") && password.equals("1")) {
-		HttpSession session=request.getSession();
-		session.setAttribute("name", "yatheesha");
-		session.setAttribute("pass", "1");
-			response.sendRedirect("welcome.jsp");
-		}
-		else
-			response.sendRedirect("login.jsp");
+		UserService us=new UserService();
+		User u=new User();
+		us.addNewUser();
+			 try {
+				u=us.getUser(name);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
+			String url="login.jsp";
+			
+			
+			if(u!=null) {
+				if(name.equals(u.getUsername()) && password.equals(u.getPassword())) {
+					HttpSession session=request.getSession();
+					session.setAttribute("name", "yatheesha");
+					session.setAttribute("pass", "1");
+					
+					
+
+						url= "welcome.jsp";
+					}	
+			}
+			
+	
+			
+			RequestDispatcher dd = request.getRequestDispatcher(url);
+
+			dd.forward(request, response);
+		
 		
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 
 
 }
